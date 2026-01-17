eea3cae7750224bbbe598244b660d1cd13733101
Job LMU <macbookpro2@MacBook-Pro-von-MacBook-2.local>
2026-01-17 13:25:18 +0300

    Fix: Remove webpack config for Turbopack compatibility

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  // Disable Turbopack to use traditional webpack compiler
-  // This resolves the "to" argument must be of type string error
-  webpack: (config, { isServer }) => {
-    return config;
-  },
 };
 
 module.exports = nextConfig;
